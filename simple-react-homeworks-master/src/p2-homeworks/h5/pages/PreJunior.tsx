c1665ee4ad7be86778aa19dcf84be3509402be53
Erzh R <erzheena8@gmail.com>
2021-02-04 23:12:38 +0700

    5 homework

--- simple-react-homeworks-master/src/p2-homeworks/h5/pages/PreJunior.tsx
+++ simple-react-homeworks-master/src/p2-homeworks/h5/pages/PreJunior.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import HW1 from "../../p2-homeworks/h1/HW1";
-import HW2 from "../../p2-homeworks/h2/HW2";
-import HW3 from "../../p2-homeworks/h3/HW3";
-import HW4 from "../../p2-homeworks/h4/HW4";
+import HW1 from "../../h1/HW1";
+import HW2 from "../../h2/HW2";
+import HW3 from "../../h3/HW3";
+import HW4 from "../../h4/HW4";
 
 
 function PreJunior() {
