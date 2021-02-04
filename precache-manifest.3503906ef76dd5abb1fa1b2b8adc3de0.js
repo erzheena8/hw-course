87f666f06876da395c3cafce888e4006e5fc429a
Erzh R <erzheena8@gmail.com>
2021-02-04 23:11:49 +0700

    Updates

---
+++ precache-manifest.3503906ef76dd5abb1fa1b2b8adc3de0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "279c3173393be625bb17e4a8426d8314",
+    "url": "/erzheena8/training.git/index.html"
+  },
+  {
+    "revision": "449a15f36fced28b4ee2",
+    "url": "/erzheena8/training.git/static/css/main.78cb00c7.chunk.css"
+  },
+  {
+    "revision": "4377c6d3c7359b28190a",
+    "url": "/erzheena8/training.git/static/js/2.953e6aa8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/erzheena8/training.git/static/js/2.953e6aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449a15f36fced28b4ee2",
+    "url": "/erzheena8/training.git/static/js/main.35a4a12f.chunk.js"
+  },
+  {
+    "revision": "19b398d9398b64cd735b",
+    "url": "/erzheena8/training.git/static/js/runtime-main.e2c7addb.js"
+  }
+]);
